a9092b562ec07fde677a6e8880cd1bd6ff8d10d9
Camilo <camilovietnam@gmail.com>
2020-12-10 14:21:20 +0700

    Simplify Dockerfile

--- install_ecc.sh
+++ install_ecc.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 # Install software dependencies
-apt-get update 
-apt-get install -y default-mysql-server nano unzip zip
+apt-get update
+apt-get install -y default-mysql-server nano unzip zip libzip-dev libicu-dev nano
+docker-php-ext-install zip mysqli pdo pdo_mysql intl
 
-## Start and configure the database
+## Enable mod_rewrite for Apache
+a2enmod rewrite
+
+## Start and configure MySQL
 /etc/init.d/mysql start
 mysql -u root -e "CREATE DATABASE eccube"
 mysqladmin -u root password '123'
 
 ## Download and configure composer
-php -r "copy('https://getcomposer.org/composer-1.phar', '/usr/bin/composer');" 
-chmod +x /usr/bin/composer 
+php -r "copy('https://getcomposer.org/composer-1.phar', '/usr/bin/composer');"
+chmod +x /usr/bin/composer
+
+## Set up local permissions
+chmod +x /var/www/html/bin/console
+cp /var/www/html/.env.dist /var/www/html/.env
+chown www-data:www-data -R /var/www/html
 
 ## Install eccube dependencies
 composer install
