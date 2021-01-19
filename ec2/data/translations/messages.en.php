8d89b0283ce1967efebbd49c95c17170f2dffe89
Camilo <camilovietnam@gmail.com>
2021-01-19 19:31:11 +0700

    Added base files

---
+++ ec2/data/translations/messages.en.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'admin' => [
+        'home' => [
+            'menu' => [
+                'basic_mgmt' => 'Basic info',
+                'product_mgmt' => 'Products',
+                'customer_mgmt' => 'Customer',
+                'order_mgmt' => 'Orders',
+                'total_mgmt' => 'Totals',
+                'newsletter_mgmt' => 'Newsletter',
+                'contents_mgmt' => 'Contents',
+                'design_mgmt' => 'Design',
+                'system_mgmt' => 'System',
+                'store_mgmt' => 'Store settings',
+            ],
+        ],
+    ],
+    'public' => [
+        'top' => [
+            'favorites' => 'Favorites',
+            'login' => 'Login',
+        ]
+    ],
+];
