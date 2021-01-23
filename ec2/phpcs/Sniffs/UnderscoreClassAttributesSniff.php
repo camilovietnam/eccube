58d96a208600329178c23b453ebec0a95eb895a3
Camilo <camilovietnam@gmail.com>
2021-01-23 12:00:11 +0700

    Sniff whitespace after comma array

--- ec2/phpcs/Sniffs/UnderscoreClassAttributesSniff.php
+++ ec2/phpcs/Sniffs/UnderscoreClassAttributesSniff.php
@@ -17,24 +17,28 @@ class UnderscoreClassAttributesSniff implements Sniff
 
     public function process(File $phpcsFile, $stackPtr)
     {
-        $tokens      = $phpcsFile->getTokens();
-        $varName     = ltrim($tokens[$stackPtr]['content'], '$');
-        $memberProps = $phpcsFile->getMemberProperties($stackPtr);
-        if (empty($memberProps) === true) {
-            // Exception encountered.
-            return;
-        }
+        try {
+            $tokens      = $phpcsFile->getTokens();
+            $varName     = ltrim($tokens[$stackPtr]['content'], '$');
+            $memberProps = $phpcsFile->getMemberProperties($stackPtr);
+            if (empty($memberProps) === true) {
+                // Exception encountered.
+                return;
+            }
 
-        $scope = ucfirst($memberProps['scope']);
-        $beginsWithUnderscore = (substr($varName, 0, 1) === '_');
+            $scope = ucfirst($memberProps['scope']);
+            $beginsWithUnderscore = (substr($varName, 0, 1) === '_');
 
-        if ($scope === 'Public' && $beginsWithUnderscore) {
-            $error = "$scope member variable \"$varName\" must not contain a leading underscore";
-            $phpcsFile->addError($error, $stackPtr, 'PublicHasUnderscore');
+            if ($scope === 'Public' && $beginsWithUnderscore) {
+                $error = "$scope member variable \"$varName\" must not contain a leading underscore";
+                $phpcsFile->addError($error, $stackPtr, 'PublicHasUnderscore');
 
-        } else if ($scope !== 'Public' && !$beginsWithUnderscore) {
-            $error = "$scope member variable \"$varName\" must contain a leading underscore";
-            $phpcsFile->addError($error, $stackPtr, 'PrivateNoUnderscore');
+            } else if ($scope !== 'Public' && !$beginsWithUnderscore) {
+                $error = "$scope member variable \"$varName\" must contain a leading underscore";
+                $phpcsFile->addError($error, $stackPtr, 'PrivateNoUnderscore');
+            }
+        } catch (\Exception $e) {
+            // ...
         }
     }
 }
