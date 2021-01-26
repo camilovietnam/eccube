d30384e964a85c3e25851880d3cc2c105453dcea
Camilo <camilovietnam@gmail.com>
2021-01-26 09:12:50 +0700

    Example config file

---
+++ ec2/data/config/config.example.php
@@ -0,0 +1,23 @@
+<?php
+define('ECCUBE_INSTALL', 'on');
+define('HTTP_URL', 'http://localhost:8888/');
+define('HTTPS_URL', 'http://localhost:8888/');
+define('ROOT_URLPATH', '/');
+define('DOMAIN_NAME', '');
+define('DB_TYPE', 'mysqli');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '123');
+define('DB_SERVER', 'eccube_db');
+define('DB_NAME', 'eccube_db');
+define('DB_PORT', '3306');
+define('ADMIN_DIR', 'admin/');
+define('ADMIN_FORCE_SSL', FALSE);
+define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
+define('AUTH_MAGIC', 'noucilaiwrouviawrouthiacriostiacraehiaja');
+define('PASSWORD_HASH_ALGOS', 'sha256');
+define('MAIL_BACKEND', 'smtp');
+define('SMTP_HOST', 'smtp.mailtrap.io');
+define('SMTP_PORT', '2525');
+define('SMTP_USER', '830fc4eecefd11');
+define('SMTP_PASSWORD', '27a1b538b621e9');
+define('UI_LANG', 'en');
