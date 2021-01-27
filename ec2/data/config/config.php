8ba730163ea767b0b150ead3f650d9fe0739d28c
Camilo <camilovietnam@gmail.com>
2021-01-27 14:48:02 +0700

    Init mysql

--- ec2/data/config/config.php
+++ ec2/data/config/config.php
@@ -13,7 +13,7 @@ define('DB_PORT', '3306');
 define('ADMIN_DIR', 'admin/');
 define('ADMIN_FORCE_SSL', FALSE);
 define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
-define('AUTH_MAGIC', 'bevourialougaitresaeliofrupuuouphaehougi');
+define('AUTH_MAGIC', 'poufraijaeraislegiowraitoujiotifrujoujia');
 define('PASSWORD_HASH_ALGOS', 'sha256');
 define('MAIL_BACKEND', 'smtp');
 define('SMTP_HOST', '');
