d7cd4c96d96c06bb63bba6c652e9612f364c6654
Camilo <camilovietnam@gmail.com>
2021-01-30 11:17:37 +0700

    Permissions, Quotes

--- ec2/data/migrations/Version20210129061344_add_layout_faq.php
+++ ec2/data/migrations/Version20210129061344_add_layout_faq.php
@@ -20,18 +20,18 @@ final class Version20210129061344_add_layout_faq extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("INSERT into dtb_pagelayout 
-        (device_type_id, page_id, page_name, url, filename, header_chk, footer_chk, edit_flg)
-        VALUES (10,29,'よくある質問・お問い合わせ','faq/index.php','faq/index',1,1,2)
-        ");
+        $this->addSql('INSERT into dtb_pagelayout'
+        . '(device_type_id, page_id, page_name, url, filename, header_chk, footer_chk, edit_flg)'
+        . 'VALUES (10,29,"よくある質問・お問い合わせ","faq/index.php","faq/index",1,1,2)'
+        );
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql("DELETE FROM dtb_pagelayout 
-        WHERE device_type_id = 10 
-        AND page_id = 29
-        ");
+        $this->addSql('ELETE FROM dtb_pagelayout'
+        . 'WHERE device_type_id = 10'
+        . 'AND page_id = 29'
+        );
     }
 }
